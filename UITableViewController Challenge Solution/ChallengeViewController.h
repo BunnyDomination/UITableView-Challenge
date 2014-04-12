f15e21c17c398c20bb00ee46a5f608f6b64d5a4a
Christina Ryan <christinaryan@Christinas-MacBook-Air.local>
2014-04-12 11:44:44 -0700

    Initial Commit

---
+++ UITableViewController Challenge Solution/ChallengeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChallengeViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Christina Ryan on 4/12/14.
+//  Copyright (c) 2014 Christina's App House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChallengeViewController : UIViewController
+
+@end
---
+++ UITableViewController Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Christina Ryan on 4/12/14.
+//  Copyright (c) 2014 Christina's App House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ChallengeAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ChallengeAppDelegate class]));
+    }
+}
